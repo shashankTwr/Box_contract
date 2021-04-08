6e3245ce457495a79aa70cd4b9729010bf0eb9e1
shashankTwr <shashanktwr007@gmail.com>
2021-04-09 01:17:51 +0530

    v2

--- contracts/Box.sol
+++ contracts/Box.sol
@@ -1,13 +1,16 @@
-pragma solidity ^0.6.0;
+pragma solidity >=0.5.0;
 
+//import openzeppelin ownable contract
+import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract Box {
+contract Box is Ownable{
   uint256 private value;
 
   //emitted when value is changed
   event ValueChanged(uint256 newValue);
 
-  function store(uint256 _value) public{
+  //add onlyOwner  modifier to restrict value of box being changed only by the owner
+  function store(uint256 _value) public onlyOwner{
   	value =  _value;
   	emit ValueChanged(value);
   }
