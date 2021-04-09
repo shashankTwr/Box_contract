7251017054bd137f29ee03442c8ae213271110cf
shashankTwr <shashanktwr007@gmail.com>
2021-04-09 15:29:16 +0530

    tests completed

--- test/Box.test.js
+++ test/Box.test.js
@@ -3,6 +3,8 @@
 
 const {expect} = require('chai');
 
+const { BN, expectEvent, expectRevert } = require('@openzeppelin/test-helpers')
+
 //Load compiled artifacts
 // artifacts saves the ABI, binary of contract and other info related to the contract saved in builds contracts folder as a json file
 
@@ -10,15 +12,28 @@ const Box = artifacts.require('Box');
 
 //Start test block
 
-contract('Box', function(){
+contract('Box', function([ owner, other]){
+	//Big number 
+	const value = new BN('42');
+
 	beforeEach(async function(){
-		this.box = await Box.new();
+		this.box = await Box.new({from:owner});
 	});
 
 	//a simple test case checking if the value returned is the same as value passed in store
 	it('retrieve returns a value previously stored', async function(){
-		await this.box.store(42);
-		value = await this.box.retrieve();
-		assert(value == '42');
+		await this.box.store(value, {from: owner});
+		expect(await this.box.retrieve()).to.be.bignumber.equal(value);
+	});
+
+	//a simple test case checking if the value returned is the same as value passed in store
+	it('stores emits an event ', async function(){
+		const reciept = await this.box.store(value, {from: owner});
+		expectEvent(reciept, 'ValueChanged',{newValue : value});
+	});
+
+	//a simple test case checking if the value returned is the same as value passed in store
+	it('Non owner cannot store a value', async function(){
+		await expectRevert(this.box.store(value, {from: other}), 'Ownable: caller is not the owner');
 	});
 });
